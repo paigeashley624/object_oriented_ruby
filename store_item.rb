07cd5bdc7e5ae976e21c8fc74b8b651e2f6dfd15
Paige Morton <paigeashley624@gmail.com>
2021-02-17 16:29:31 -0400

    continued edits for exercise 2

--- store_item.rb
+++ store_item.rb
@@ -1,15 +1,20 @@
-avocado = {color: "green", price => 1, isle => 18}
-oranges = {color: "orange", price: 3, isle: 5}
-potatoe = {color: "brown", price: 4, isle: 6}
+item1 = {name: "avocado", color: "green", price: 1, isle => 18}
+item2 = {name: "orange", color: "orange", price: 3, isle: 5}
+item3 = {name: "potato" color: "brown", price: 4, isle: 6}
 
 class Store
-  attr_reader :color, :isle
+  attr_reader :name, :color, :isle
   attr_writer :price
 
-  def initialize(input_color, input_price, input_isle) 
+  def initialize(input_name, input_color, input_price, input_isle) 
+    @name = input_name
     @color = input_color
     @price = input_price
     @isle = input_isle
+  end
+
+  def print_info
+    puts "#{name} #{color} cost #{price} located on isle #{isle} "
 
 end
 
