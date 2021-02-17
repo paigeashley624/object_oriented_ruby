8f20750441639afca9982ab25b4fc46b30488095
Paige Morton <paigeashley624@gmail.com>
2021-02-17 16:14:16 -0400

    partial solution to exercise 2

--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,15 @@
-avocado = {"color" => "green", "price" => 1, "isle" => 18}
+avocado = {color: "green", price => 1, isle => 18}
 oranges = {color: "orange", price: 3, isle: 5}
-apple = {color: "green", price: 4, isle: 6}
-p avocado["color"]
-p oranges[:price]
+potatoe = {color: "brown", price: 4, isle: 6}
+
+class Store
+  attr_reader :color, :isle
+  attr_writer :price
+
+  def initialize(input_color, input_price, input_isle) 
+    @color = input_color
+    @price = input_price
+    @isle = input_isle
+
+end
+
