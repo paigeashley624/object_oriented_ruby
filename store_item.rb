54d6108985e1d93948fbc7a3ecb3b69034f56613
Paige Morton <paigeashley624@gmail.com>
2021-02-17 14:34:11 -0400

    creates array

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+avocado = {"color" => "green", "price" => 1, "isle" => 18}
+oranges = {color: "orange", price: 3, isle: 5}
+apple = {color: "green", price: 4, isle: 6}
+p avocado["color"]
+p oranges[:price]
